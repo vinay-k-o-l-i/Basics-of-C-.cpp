1326cf78e29f41187b96494effc6ffd4c6c5ccaf
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-07-14 22:58:46 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;    //vinay koli,133,entc
+
+int main(){
+    float num1, num2, sum, Div, sub;
+
+    cout << "enter the 1 num1 :";
+    cin >> num1;
+
+    cout << "enter the 2 num2 :";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << sum << endl;
+
+    Div = num1 / num2;
+    cout << Div << endl;
+
+    sub = num1 - num2;
+    cout << sub << endl;
+
+    return 0;
+}
+/*enter the 1 num1:2342.5443364
+enter the 2 num2:4545.3555634
+6888.9
+0.515591
+-2201.81*/
